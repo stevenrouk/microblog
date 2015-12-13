ffcb8db458201e9b2441d0a132e9c0f4c4493034
Steven Rouk <stevenrouk@gmail.com>
2015-12-13 13:14:09 -0700

    complete part II of the flask mega tutorial

--- app/views.py
+++ app/views.py
@@ -1,6 +1,28 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!',
+            'timestamp': '2 minutes ago'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!',
+            'timestamp': '5 minutes ago'
+        },
+        {
+            'author': {'nickname': 'Liz'},
+            'body': 'What a dayyyy, what a day!',
+            'timestamp': '1 hour ago'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
